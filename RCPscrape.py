7c14f261b8110b5d551e9ff4f97b6db4dbf7b2b5
unknown <camden.blatchly@thedatatrust.com>
2018-04-06 12:11:44 -0400

    Scraper with Sponsor, Source, and Population added

--- RCPscrape.py
+++ RCPscrape.py
@@ -1,3 +1,11 @@
+# --------------------------------------------------- #
+#
+# Poll Scraper: gets the latest 2018 Midterm election
+# polls from the Real Clear Politics website, then 
+# updates csv file with latest polls.
+#
+# --------------------------------------------------- #
+
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 import ssl
@@ -10,72 +18,97 @@ from datetime import datetime
 ssl._create_default_https_context = ssl._create_unverified_context
 
 my_url = "https://realclearpolitics.com/epolls/latest_polls/"
+my_url_2 = "https://realclearpolitics.com/epolls/other/2018_generic_congressional_vote-6185.html#polls"
+my_url_3 = "https://projects.fivethirtyeight.com/pollster-ratings/"
 
 # opening up connnection, grabbing the page
 
 uClient = uReq(my_url)
+uClient_2 = uReq(my_url_2)
+uClient_3 = uReq(my_url_3)
 
 # offloads connection content into a variable
 
 page_html = uClient.read()
+page_html_2 = uClient_2.read()
+page_html_3 = uClient_3.read()
 
 # closes connection
 
 uClient.close()
+uClient_2.close()
+uClient_3.close()
 
 # html parsing
 
 page_soup = soup(page_html, "html.parser")
+page_soup_2 = soup(page_html_2, "html.parser")
+page_soup_3 = soup(page_html_3, "html.parser")
 
 # grabs each product
 
-races = page_soup.findAll("td", {"class":"lp-race"})
-raceC = page_soup.findAll("td", {"class":"lp-race"})
-polls = page_soup.findAll("td", {"class":"lp-poll"})
-results = page_soup.findAll("td", {"class":"lp-results"})
-spreads = page_soup.findAll("td", {"class":"lp-spread"})
-dates = page_soup.findAll("td", {"class":"date"})
-tables = page_soup.findAll("table", {"class":"sortable"})
+poll_races = page_soup.findAll("td", {"class":"lp-race"})
+poll_races_count = page_soup.findAll("td", {"class":"lp-race"})
+poll_names = page_soup.findAll("td", {"class":"lp-poll"})
+poll_results = page_soup.findAll("td", {"class":"lp-results"})
+poll_spreads = page_soup.findAll("td", {"class":"lp-spread"})
+table_dates = page_soup.findAll("td", {"class":"date"})
+poll_tables = page_soup.findAll("table", {"class":"sortable"})
 
 
-# Opens an output file to write to, writes header
-file_object = open("RCPinfo.txt", "w")
-file_object.write('%-70s %-35s %-95s %-30s %-30s\n\n' % ("Race Name", "Poll Name", "Poll Results", "Poll Spread", "Date"))
+# grabs poll population from seperate Real Clear Politics page
+
+poll_populations = page_soup_2.findAll("td", {"class":"sample"})
+
+# grabs poll rating info from 538
+
+print(page_soup_3)
 
 
-pollCountList = []
-mapList = []
 
-# append the number of races in this table
-for table in tables:
-	names = table.findAll('td', {"class":"lp-race"})
-	pollCountList.append(len(names))
+# holds size of each poll table
+tableLengthList = []
 
-rowNum = len(raceC)
-var = len(pollCountList)
+# holds the table index for every poll
+tableIndexList = []
 
-# We loop through the total number of polls
-# for every poll we create a map for the table of the poll
+# holds date of each table
+tableDateList = []
 
-for y in range(0, var):
-	c = 0
-	while c < pollCountList[y]:
-		mapList.append(y)
-		c = c + 1
+# holds scraped polls
+scrapedPollsList = []
 
-dateList = []
+# holds polls already in the csv
+oldPollsList = []
 
-# append all the dates to a list
-for date in dates:
-	dateList.append(date.string);
+# list to hold the dates of the polls currently in the csv
+oldDatesList = []
 
-# list to hold current list of scraped polls
-allPolls = []
-# list to hold the current polls in the csv
-oldPolls = []
 
-# Takes date format like: Wednesday, February 7
-# transforms into format DD/MM/YYYY
+# store the number of races in each table in tableLengthList
+for table in poll_tables:
+	polls_in_table = table.findAll('td', {"class":"lp-race"})
+	tableLengthList.append(len(polls_in_table))
+
+# number of rows on the RCP website
+total_row_num = len(poll_races_count)
+# number of tables on the RCP website
+table_num = len(tableLengthList)
+
+# calculate table index for every poll
+for table_index in range(0, table_num):
+	table_row_count = 0
+	while table_row_count < tableLengthList[table_index]:
+		tableIndexList.append(table_index)
+		table_row_count = table_row_count + 1
+
+# store table dates
+for date in table_dates:
+	tableDateList.append(date.string);
+
+
+# Takes date format, Example: Wednesday, February 7
+# transforms into format: DD/MM/YYYY
 def formatDate (str):
 	date = ""
 	a,b = str.split(',')
@@ -109,82 +142,124 @@ def formatDate (str):
 		date = date + "12/"
 
 	date = date + day + "/"
-
 	year = datetime.now().strftime('%Y')
-
 	date = date + year
-
 	return date
 
 # Take as input: Democrats 44, Republicans 39
-# Transform to a simple decimal value
+# Transforms input into a simple decimal value
 def formatDem (str):
-	result = ""
 	Dem,Rep= str.split(',')
-	a,DemP,c,RepP=str.split(' ')
-	del a
-	del c
-	Score,comma=DemP.split(',')
+	DemName,Dem_percent,RepName,Rep_percent=str.split(' ')
+	del DemName
+	del RepName
+	Dem_percent,comma=Dem_percent.split(',')
 	del comma
-	Score = "." + Score
-	return Score
+	Dem_percent = "." + Dem_percent
+	return Dem_percent
 
 def formatRep (str):
-	result = ""
 	Dem,Rep= str.split(',')
-	a,DemP,c,RepP=str.split(' ')
-	del a
-	del c
-	RepP = "." + RepP
-	return RepP
+	DemName,Dem_percent,RepName,Rep_percent=str.split(' ')
+	del DemName
+	del RepName
+	Rep_percent = "." + Rep_percent
+	return Rep_percent
 
-# list to hold the dates of the polls currently in the csv
-oldDates = []
+# Takes split input like "Economist/YouGov"
+# returns poll source
+def formatSource (str):
+	slash_index = str.find('/')
 
-# append all of the dates into oldDates
+	poll_sponsor = ""
+	poll_source = ""
+
+	if slash_index > 0:
+		poll_sponsor,poll_source=str.split('/')
+	else:
+		poll_source = str
+
+	return poll_source
+
+# Takes split input like "Economist/YouGov"
+# returns poll sponsor
+def formatSponsor (str):
+	slash_index = str.find('/')
+
+	poll_sponsor = ""
+	poll_source = ""
+
+	if slash_index > 0:
+		poll_sponsor,poll_source=str.split('/')
+	else:
+		poll_sponsor = ""
+
+	return poll_sponsor
+
+# Takes poll population input like "1246 RV"
+# transforms to Registered voters
+def formatPopulation (str):
+
+	space_index = str.find(' ')
+
+	pop_size = ""
+	pop_type = ""
+
+	if space_index > 0:
+		pop_size,pop_type=str.split(' ')
+	else:
+		pop_type = str
+
+	if pop_type == "RV":
+		return "Registered Voters"
+	if pop_type == "A":
+		return "Adults"
+	if pop_type == "LV":
+		return "Likely Voters"
+
+
+# store dates of polls currently in the csv
 with open("outputFile.csv", "r", encoding="utf8") as fp:
 	reader = csv.reader(fp)
 	for row in reader:
 		if (len(row) > 0):
-			oldDates.append(row[0])	
+			oldDatesList.append(row[0])	
 	
 # find the most recent date in the current csv
-max = oldDates[-1]
+most_recent_date = oldDatesList[-1]
 
 
-for count in range(0, rowNum):
-	raceName = races[count].a.string
-	pollName = polls[count].a.string
-	pollSpread = spreads[count].span.string
-	pollResult = results[count].a.string
+for count in range(0, total_row_num):
 
-	pollDate = dateList[mapList[count]]
+	raceName = poll_races[count].a.string
+	pollName = poll_names[count].a.string
+	pollSpread = poll_spreads[count].span.string
+	pollResult = poll_results[count].a.string
+
+	pollSponsor = formatSponsor(pollName)
+	pollSource = formatSource(pollName)
+
+	pollDate = tableDateList[tableIndexList[count]]
 	pollDate = formatDate(pollDate)
+
+	pollPopulation = poll_populations[count + 1].string
+	pollPopulation = formatPopulation(pollPopulation)
 
 	if raceName == "2018 Generic Congressional Vote":
 		RepScore = formatRep(pollResult)
 		DemScore = formatDem(pollResult)
 
-		pollInstance = [pollDate, DemScore, RepScore, pollName, raceName]
-		allPolls.append(pollInstance)
+		pollInstance = [pollDate, DemScore, RepScore, pollPopulation, pollSponsor, pollSource]
+		scrapedPollsList.append(pollInstance)
 
 # reverse so that the data gets input with oldest at bottom and newest at top
-allPolls.reverse()
+scrapedPollsList.reverse()
 
 # write new polls to the csv
 with open("outputFile.csv", "a", newline='') as f:
 	writer=csv.writer(f, delimiter=',')
-	for poll in allPolls:
-		pollDate1 = time.strptime(poll[0], "%m/%d/%Y")
-		max1 = time.strptime(max, "%m/%d/%Y")
-		if pollDate1 > max1:
+	for poll in scrapedPollsList:
+		poll_date_formatted = time.strptime(poll[0], "%m/%d/%Y")
+		most_recent_date_formatted = time.strptime(most_recent_date, "%m/%d/%Y")
+		if poll_date_formatted > most_recent_date_formatted:
 			writer.writerow(poll)
-
-
-file_object.close()
-
-# test file for only the most recent polls
-with open("example.csv", "w", newline='') as csvfile:
-	writer = csv.writer(csvfile, delimiter=',')
-	for poll in allPolls:
-		writer.writerow(poll)
